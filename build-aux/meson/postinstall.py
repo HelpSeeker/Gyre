35bde5a17d6dbd3ca4c70ecfd8613efeeab88ac2
HelpSeeker <AlmostSerious@protonmail.ch>
2021-01-01 21:32:04 +0100

    Update postinstall script to work on Windows
    Not much of an improvement, but at least you don't have to compile the
    schema and update the icon cache manually when trying to run Gyre on
    Windows.

--- build-aux/meson/postinstall.py
+++ build-aux/meson/postinstall.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python3
 
-from os import environ, path
+import os
 from subprocess import call
 
-prefix = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
-datadir = path.join(prefix, 'share')
-destdir = environ.get('DESTDIR', '')
+prefix = os.environ.get('MESON_INSTALL_PREFIX', '/usr/local')
+datadir = os.path.join(prefix, 'share')
+destdir = os.environ.get('DESTDIR', '')
 
-# Package managers set this so we don't need to run
-if not destdir:
+# Assume msys2 (mingw64) environment on Windows
+if os.name == "nt":
     print('Updating icon cache...')
-    call(['gtk-update-icon-cache', '-qtf', path.join(datadir, 'icons', 'hicolor')])
-
-    print('Updating desktop database...')
-    call(['update-desktop-database', '-q', path.join(datadir, 'applications')])
+    call(['gtk-update-icon-cache-3.0.exe', '-qtf', os.path.join(datadir, 'icons', 'hicolor')])
 
     print('Compiling GSettings schemas...')
-    call(['glib-compile-schemas', path.join(datadir, 'glib-2.0', 'schemas')])
+    call(['glib-compile-schemas.exe', os.path.join(datadir, 'glib-2.0', 'schemas')])
+# Package managers set this so we don't need to run
+elif not destdir:
+    print('Updating icon cache...')
+    call(['gtk-update-icon-cache', '-qtf', os.path.join(datadir, 'icons', 'hicolor')])
 
+    print('Updating desktop database...')
+    call(['update-desktop-database', '-q', os.path.join(datadir, 'applications')])
 
+    print('Compiling GSettings schemas...')
+    call(['glib-compile-schemas', os.path.join(datadir, 'glib-2.0', 'schemas')])
